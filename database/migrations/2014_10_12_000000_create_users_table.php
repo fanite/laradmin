ffc51f32bd7770b20f455d43b0d35a5054b17825
黄锡平 <huangxiping@huaqin.com>
2019-12-26 17:55:46 +0800

    大萨达撒多

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('staff_code', 100)->unique()->comment('工号');
+            $table->string('name')->comment('名字');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('phone')->comment('电话号码');
+            $table->enum('gender', ['male', 'female'])->nullable()->comment('性别');
+            $table->string('work_place', 100)->comment('工作地点');
+            $table->text('avatar')->comment('头像地址');
             $table->string('password');
+            $table->ipAddress('last_ip')->comment('最后登录的IP地址');
+            $table->boolean('is_admin')->comment('超级账户');
+            $table->string('user_type', 100)->nullable(false)->comment('账户类型');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
