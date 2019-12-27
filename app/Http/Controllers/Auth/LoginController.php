455855caa25b97e04b5da85b211f95aa29d29572
Fanite <bellong@vip.qq.com>
2019-12-27 23:12:26 +0800

    ggg

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,41 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Login action.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $request->request->add($this->defaultClient());
+
+        $proxy = Request::create('oauth/token', 'POST', $request->all());
+
+        return Route::dispatch($proxy);
+    }
+
+    public function logout(Request $request)
+    {
+        return $request->user()->token()->revoke();
+    }
+
+    /**
+     * Get the default passport client for current app.
+     *
+     * @param Boolean $isRefreshType
+     * @return Array $client
+     */
+    protected function defaultClient(bool $isRefreshType = false)
+    {
+        $client = config('password.default_client');
+        if ($isRefreshType) {
+            $client = array_merge($client, ['grant_type' => 'refresh_token']);
+        }
+        return $client;
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->registerPassportRoutes();
+    }
+
+    /**
+     * register passport routes
+     *
+     * @return void
+     */
+    protected function registerPassportRoutes()
+    {
+        Passport::routes();
     }
 }
